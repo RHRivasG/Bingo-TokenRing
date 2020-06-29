8bed5f371e4fa35df4033f6707bcebd964e7912d
RHRivasG <51801113+RHRivasG@users.noreply.github.com>
2020-06-29 07:30:27 -0400

    ball

--- logic/items/ball/blower.go
+++ logic/items/ball/blower.go
@@ -1,9 +1,34 @@
 package items
 
+import (
+	"math/rand"
+)
+
+//Blower .
 type Blower struct {
-	balls []Ball
+	Balls []Ball
 }
 
-func GetBallOut() {
+//New .
+func New() Blower {
+	vingu := []string{"V", "I", "N", "G", "U"}
+	var balls []Ball
+	for i, letter := range vingu {
+		for j := i*15 + 1; j <= i*15+15; j++ {
+			balls = append(balls, Ball{letter, j})
+		}
+	}
+	return Blower{balls}
+}
 
+//GetBallOut .
+func (b *Blower) GetBallOut() Ball {
+	n := rand.Intn(len(b.Balls))
+	ball := b.Balls[n]
+	b.Balls = remove(b.Balls, n)
+	return ball
+}
+
+func remove(balls []Ball, i int) []Ball {
+	return append(balls[:i], balls[i+1:]...)
 }
