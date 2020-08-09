b3f164a992e7c956cf1603539576c0b1e46a4414
RHRivasG <51801113+RHRivasG@users.noreply.github.com>
2020-08-09 01:07:30 -0400

    factory

--- logic/factories/bingo-factory.go
+++ logic/factories/bingo-factory.go
@@ -1,4 +1,22 @@
 package factories
 
-func BingoFactory() {
+import (
+	"bingo-tokenring/logic/items"
+	"bingo-tokenring/protocol"
+	"log"
+)
+
+//BingoFactory .
+func BingoFactory(listener string, writer string, numBoards int) (protocol.Protocol, []items.Board, items.Blower) {
+	protocol, err := protocol.NewProtocol(listener, writer)
+	if err != nil {
+		log.Fatal(err)
+	}
+	var boards []items.Board
+	for i := 0; i < numBoards; i++ {
+		boards[i] = items.NewBoard("")
+	}
+	blower := items.NewBlower()
+	return protocol, boards, blower
+
 }
