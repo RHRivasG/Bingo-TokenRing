e6dad9266b3d4a0089f2b953b0ee03deff5c5c1f
RHRivasG <51801113+RHRivasG@users.noreply.github.com>
2020-08-11 02:55:18 -0400

    Update main.go

--- main.go
+++ main.go
@@ -2,74 +2,77 @@ package main
 
 import (
 	"bingo-tokenring/logic"
-	"fmt"
+	"log"
 	"os"
+	"strconv"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
+
+	i, err := strconv.Atoi(os.Args[5])
+	if err != nil {
+		log.Fatal(err)
+	}
+	g := logic.NewGame(os.Args[2], os.Args[3], i, os.Args[4])
+	r := gin.Default()
+	r.Use(cors.Default())
+
+	//Routes
+	r.GET("init", func(ctx *gin.Context) {
+		g.Init()
+	})
+	r.GET("loadgame", func(ctx *gin.Context) {
+		g.LoadGame(ctx)
+	})
+	r.GET("update", func(ctx *gin.Context) {
+		g.Update(ctx)
+	})
+	r.GET("send", func(ctx *gin.Context) {
+		g.Send()
+	})
+	r.GET("wait", func(ctx *gin.Context) {
+		g.Wait()
+	})
+	r.GET("close", func(ctx *gin.Context) {
+		g.Close()
+	})
+
+	r.Run(":" + os.Args[1])
 	/*
-		if os.Args[1:] != nil {
-			protocol, err := protocol.NewProtocol(os.Args[1], os.Args[2])
-			if err != nil {
-				log.Fatal(err)
-			}
-			var arbitro []string
-			arbitro = append(arbitro, os.Args[2])
-			messages, err := protocol.Converse(arbitro)
-			if err != nil {
-				log.Fatal(err)
-			}
-			if messages[0] == protocol.GetWriterName() {
-				fmt.Println(protocol)
-			} else {
-				fmt.Println(messages[0] + " es el arbitro")
-				protocol.Write(messages)
-			}
-			if messages[0] == protocol.GetWriterName() {
-				fmt.Println("H")
-				protocol.Write([]string{"Hola"})
-			} else {
-				res, err := protocol.Listen()
-				if err != nil {
-					log.Fatal(err)
-				}
-				fmt.Println(res)
-			}
+		g := logic.NewGame(os.Args[2], os.Args[3], 2, "lineal")
+		g.Boards[0].Tiles[0][3].Taken = true
+		g.Boards[0].Tiles[1][3].Taken = true
+		g.Boards[0].Tiles[2][3].Taken = true
+		g.Boards[0].Tiles[3][3].Taken = true
+		g.Boards[0].Tiles[4][3].Taken = true
+		g.Boards[1].Tiles[0][3].Taken = true
+		g.Boards[1].Tiles[1][3].Taken = true
+		g.Boards[1].Tiles[2][3].Taken = true
+		g.Boards[1].Tiles[3][3].Taken = true
+		g.Boards[1].Tiles[4][3].Taken = true
+		g.LoadGame()
+		g.Init()
+		if g.Director {
+			g.Update()
+			fmt.Println(g.Message)
+			g.Send()
+			g.Wait()
+			g.Update()
+			fmt.Println(g.Message)
+			g.Send()
 
 		} else {
-			fmt.Println("No hay argumentos")
-		}
-	*/
-	/*
-		i, err := strconv.Atoi(os.Args[5])
-		if err != nil {
-			log.Fatal(err)
-		}
-		g := logic.NewGame(os.Args[2], os.Args[3], i, os.Args[4])
-		r := gin.Default()
-		r.Use(cors.Default())
-
-		//Routes
-		r.GET("init", func(ctx *gin.Context) {
-			g.Init()
-		})
-		r.GET("loadgame", func(ctx *gin.Context) {
-			g.LoadGame()
-		})
-		r.GET("update", func(ctx *gin.Context) {
-			g.Update(ctx)
-		})
-		r.GET("send", func(ctx *gin.Context) {
+			fmt.Println(g.Message)
+			g.Update()
+			fmt.Println(g.Message)
 			g.Send()
-		})
-		r.GET("wait", func(ctx *gin.Context) {
 			g.Wait()
-		})
-
-		r.Run(":" + os.Args[1])
-	*/
-	g := logic.NewGame(os.Args[2], os.Args[3], 2, "lineal")
-	g.LoadGame()
-	g.Close()
-	fmt.Println(g.Boards)
+			g.Update()
+			fmt.Println(g.Message)
+			g.Send()
+		}
+		g.Close()*/
 }
